803d04b5b0d8af16bbd5ab44584cb89b60b31ed5
一中 陳 <ez945y@gmail.com>
2023-03-18 09:29:41 +0800

    movement 3

--- app/src/main/java/ntutifm/game/rabbit/MainActivity.kt
+++ app/src/main/java/ntutifm/game/rabbit/MainActivity.kt
@@ -22,6 +22,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ntutifm.game.rabbit.ui.theme.RabbitTheme
+import java.util.ArrayDeque
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,7 +66,7 @@ fun Greeting() {
         val dog1 = remember { mutableStateOf(1) }
         val dog2 = remember { mutableStateOf(3) }
         val dog3 = remember { mutableStateOf(5) }
-        val rabbit = remember { mutableStateOf(41) }
+        val rabbit = remember { mutableStateOf(13) }
         val current = remember { mutableStateOf(50) }
         val current2 = remember { mutableStateOf(50) }
         val pic01 = remember { mutableStateOf(R.drawable.dog) }
@@ -77,10 +80,10 @@ fun Greeting() {
         val pic31 = remember { mutableStateOf(R.drawable.dot) }
         val pic32 = remember { mutableStateOf(R.drawable.dot) }
         val pic41 = remember { mutableStateOf(R.drawable.rabbit) }
-        val dogs = mutableListOf(
-            1,
-            3,
-            5)
+        val dogs = arrayListOf(
+            dog1.value,
+            dog2.value,
+            dog3.value)
         val pics = mutableListOf(
             pic01,
             pic01,
@@ -96,25 +99,7 @@ fun Greeting() {
             pic32,
             pic41,
             pic41)
-        val arr = arrayListOf(15,
-            16,
-            19,
-            20,
-            23,
-            26,
-            29,
-            31,
-            36,
-            38,
-            40,
-            43,
-            45,
-            46,
-            47,
-            -1,
-            10,
-            11,
-            12,
+        val arrD = arrayListOf(15,16,19,20,23,26,29,31,36,38,40,43,45,46,47,-1,10,11,12,
             -1,
             11,
             20,
@@ -144,6 +129,8 @@ fun Greeting() {
             -1,
             -1,
             -1)
+        val arrR = arrayListOf(15,16,17,18,22,26,30,33,41,44,48,52,56,57,60,-1,-1,-1,20,21,11,1,21,10,12,1,21,22,11,1,30,21,10,30,31,32,20,22,10,11,12,32,21,12,41,31,20,21,41,30,32,21,41,31,21,22,-1,30,31,32,-1)
+
         if (current.value != dog1.value && current.value != dog2.value && current.value != dog3.value) {
             pics[dog1.value].value = R.drawable.dog
             pics[dog2.value].value = R.drawable.dog
@@ -159,43 +146,94 @@ fun Greeting() {
                 pics[dog3.value].value = R.drawable.dogselect
             }
         }
+        if (current2.value != rabbit.value) {
+            pics[rabbit.value].value = R.drawable.rabbit
+        } else {
+            if (current2.value == rabbit.value) {
+                pics[rabbit.value].value = R.drawable.rabbitselect
+            }
+        }
 
+        fun re(){
+            for(item in pics){
+            if (item.value == R.drawable.selected) {
+                item.value = R.drawable.dot
+            }
+            if (item.value == R.drawable.dogselect) {
+                item.value = R.drawable.dog
+            }
+            if (item.value == R.drawable.rabbitselect) {
+                item.value = R.drawable.rabbit
+            }}
+        }
         fun indexOf(array:MutableList<Int>,element:Int):Int{
             array.indexOf(element)
             return -1
         }
+        var flag = remember{ mutableStateOf(true)}
         fun movement(picNum: Int){
-            if (current.value != picNum && pics[picNum].value == R.drawable.selected) {
-                //if(current.value in dogs){
-                //    Log.d("mm","${dogs.indexOf(current.value)}")
-                //}
-                //dogs[dogs.indexOf(current.value)]= picNum
-                dog2.value = picNum
-                pics[current.value].value = R.drawable.dot
-                (arr[current.value]until arr[current.value+1]).forEach {
-                    if (pics[arr[it].div(10)*3 + arr[it].mod(10)].value == R.drawable.selected) {
-                        pics[arr[it].div(10)*3 + arr[it].mod(10)].value = R.drawable.dot //此處要查圖
+
+            if(flag.value){ //狗子
+                if (current.value != picNum && pics[picNum].value == R.drawable.selected) {
+                    when(dogs.indexOf(current.value)){
+                        0 -> dog1.value = picNum
+                        1 -> dog2.value = picNum
+                        2 -> dog3.value = picNum
                     }
-                }
-                current.value = 50
-            } else {
-                if (pics[picNum].value == R.drawable.dog) {
-                    current.value = picNum
-                    (arr[picNum]until arr[picNum+1]).forEach {
-                        if (arr[it].div(10)*3 + arr[it].mod(10) != dog1.value && arr[it].div(10)*3 + arr[it].mod(10) != dog2.value && arr[it].div(10)*3 + arr[it].mod(10) != dog3.value && arr[it] != rabbit.value) {
-                            pics[arr[it].div(10)*3 + arr[it].mod(10)].value = R.drawable.selected //此處要查圖
-                        }
-                    }
+                    dogs[dogs.indexOf(current.value)]= picNum
+
+                    pics[current.value].value = R.drawable.dot
+                    re()
+                    current.value = 50
+                    flag.value = false
                 } else {
-                    if (pics[picNum].value == R.drawable.dogselect) {
-                        current.value = 50
-                        (arr[picNum]until arr[picNum+1]).forEach {
-                            if (pics[arr[it].div(10)*3 + arr[it].mod(10)].value == R.drawable.selected) {
-                                pics[arr[it].div(10)*3 + arr[it].mod(10)].value = R.drawable.dot //此處要查圖
+                    if (pics[picNum].value == R.drawable.dog) {
+                        current.value = picNum
+                        re()
+                        (arrD[picNum]until arrD[picNum+1]).forEach {
+                            var linkPoint = arrD[it].div(10)*3 + arrD[it].mod(10)
+    //                        stack.push(linkPoint)
+                            if (dogs.indexOf(linkPoint) < 0 && linkPoint != rabbit.value) {
+                                pics[linkPoint].value = R.drawable.selected //此處要查圖
                             }
                         }
+
+                    } else {
+                        if (pics[picNum].value == R.drawable.dogselect) {
+                            current.value = 50
+                            re()
+                        }
                     }
                 }
+            }else { //兔子
+                if (current2.value != picNum && pics[picNum].value == R.drawable.selected) {
+                    rabbit.value = picNum
+                    pics[current2.value].value = R.drawable.dot
+                    re()
+                    current2.value = 50
+                    flag.value = true
+                } else {
+                    if (pics[picNum].value == R.drawable.rabbit) {
+                        current2.value = picNum
+                        re()
+                        (arrR[picNum]until arrR[picNum+1]).forEach {
+                            var linkPoint = arrR[it].div(10)*3 + arrR[it].mod(10)
+                            //                        stack.push(linkPoint)
+                            if (dogs.indexOf(linkPoint) < 0 && linkPoint != rabbit.value) {
+                                pics[linkPoint].value = R.drawable.selected //此處要查圖
+                            }
+                        }
+
+                    } else {
+                        if (pics[picNum].value == R.drawable.rabbitselect) {
+                            current2.value = 50
+                            re()
+                        }
+                    }
+                }
+            }
+            if((dogs[0]+dogs[1]+dogs[2]).div(10).div(3) < rabbit.value - 1){
+                //Rabbit win
             }
         }
         //1
